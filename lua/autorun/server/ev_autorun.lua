618d56a0f7094fcbf28e0cd3f884388fd95f4dcc
Nils W <Nils W>
2017-03-05 17:43:24 +0100

    changed load order, added check for vON

--- lua/autorun/server/ev_autorun.lua
+++ lua/autorun/server/ev_autorun.lua
@@ -1,26 +1,28 @@
-/*-------------------------------------------------------------------------------------------------------------------------
+--[[-------------------------------------------------------------------------------------------------------------------------
 	Serverside autorun file
--------------------------------------------------------------------------------------------------------------------------*/
+-------------------------------------------------------------------------------------------------------------------------]]--
 
-// Set up Evolve table
+-- Set up Evolve table
 evolve = {}
 
-// Distribute clientside and shared files
+-- Load vON
+local von_bak = von
+if not von then include( "../../includes/ev_von/von.lua") end
+if not von then error( "Evolve loading aborted. vON not found!" )
+
+evolve.von = von
+von = von_bak
+
+-- Distribute clientside and shared files
 AddCSLuaFile( "autorun/client/ev_autorun.lua" )
 AddCSLuaFile( "ev_framework.lua" )
 AddCSLuaFile( "ev_cl_init.lua" )
 AddCSLuaFile( "ev_menu/cl_menu.lua" )
 
-// Load serverside files
---Load vON
-local von_bak = von
-include( "../../includes/ev_vON/von.lua")
-evolve.von = von
-von = von_bak
-
+-- Load serverside files
 include( "ev_framework.lua" )
 include( "ev_sv_init.lua" )
 include( "ev_menu/sv_menu.lua" )
 
-// SourceBans integration
+-- SourceBans integration
 include( "ev_sourcebans.lua" )
